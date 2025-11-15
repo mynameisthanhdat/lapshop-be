056200243b836e17f9779403a3242b46fece12b8
DatPham <dat.thanh@themartec.com>
2025-11-15 19:05:01 +0700

    set protect for edit delete product and order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, getOrders, getOrderById, updateOrderById, deleteOrderById, getOrdersByUserId } = require("../controllers/orderController");
+const {
+  createOrder,
+  getOrders,
+  getOrderById,
+  updateOrderById,
+  deleteOrderById,
+  getOrdersByUserId,
+} = require("../controllers/orderController");
+const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 router.get("/admin/order", getOrders);
 router.post("/order", createOrder);
 router.get("/order/:userId", getOrdersByUserId);
 router.get("/admin/order/:orderId", getOrderById);
-router.put("/admin/order/:orderId", updateOrderById);
-router.delete("/admin/order/:orderId", deleteOrderById);
+router.put("/admin/order/:orderId", protect, adminOnly, updateOrderById);
+router.delete("/admin/order/:orderId", protect, adminOnly, deleteOrderById);
 
 module.exports = router;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, getAllProducts, createProduct, getProductById, deleteProductById, updateProductById } = require("../controllers/productController");
+const {
+  getProducts,
+  getAllProducts,
+  createProduct,
+  getProductById,
+  deleteProductById,
+  updateProductById,
+} = require("../controllers/productController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
 router.get("/product/all", getAllProducts);
 router.get("/product", getProducts);
 router.post("/product", protect, adminOnly, createProduct);
 router.get("/product/:productId", getProductById);
-router.put("/product/:productId", updateProductById);
-router.delete("/product/:productId", deleteProductById);
+router.put("/product/:productId", protect, adminOnly, updateProductById);
+router.delete("/product/:productId", protect, adminOnly, deleteProductById);
 
 module.exports = router;
